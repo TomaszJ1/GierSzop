dda571d56ad87f58b34552706faaebb70cb02677
Max <maksymilian.marys@gmail.com>
2024-02-27 15:58:34 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ import GameList from "./components/GameList";
 import "./styles/App.css"
 
 const App = () => {
+
+  // https://semaphoreci.com/blog/routing-layer-react
+  const PathConstants = {
+    // MAIN: "/",
+    GAMES: "/games",
+    ABOUT: "/about",
+    CONTACT: "/contact",
+  }
+  const routes = [
+    // example { path: PathConstants.TEAM, element: <TeamPage /> },
+    { path: PathConstants.GAMES, element: <GameList games={games}/> },
+    // ...
+  ]
+
   
   const [games, setGames] = useState([])
 
